8c3bcde96c2a048347758df752baea35152ac4a4
Patrick Ellenberger <p.ellenberger@outlook.com>
2016-02-01 14:03:55 +0100

    Add paging / sorting and SQL initialization

--- src/main/java/ch/admin/seco/jobroom/web/JobPositionRepository.java
+++ src/main/java/ch/admin/seco/jobroom/web/JobPositionRepository.java
@@ -3,8 +3,9 @@ package ch.admin.seco.jobroom.web;
 
 import ch.admin.seco.jobroom.model.JobPosition;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // TODO enable "security" (authentication control) and "optimistic locking"
 @RepositoryRestResource(collectionResourceRel = "job", path = "job")
-public interface JobPositionRepository extends CrudRepository<JobPosition, Long> {}
+public interface JobPositionRepository extends PagingAndSortingRepository<JobPosition, Long> {}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO job_position (id, title, description, company_name, company_country_code, company_locality, company_postal_code, company_street, company_house_number, start_immediate)
+-- VALUES (1, 'John Doe', '...........hey!
+-- more blah,...........', 'SQL Power', 'CA', 'Montreal Nord', 'QC H1G 2V1', 'Hockey Avenue', '101', 0);
+-- INSERT INTO job_position_language_skill(job_position_id, language, spoken_level, written_level)
+-- VALUES (1, 'fr', 1, 0);
+-- INSERT INTO job_position_language_skill(job_position_id, language, spoken_level, written_level)
+-- VALUES (1, 'en', 2, 2);
