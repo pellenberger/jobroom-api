4a8a0bc1f3682ee12fdd8509bb019c85ccde8c01
Patrick Ellenberger <p.ellenberger@outlook.com>
2016-02-10 17:27:11 +0100

    Add helpers to provide testable data

---
+++ src/test/java/ch/admin/seco/jobroom/JobOfferTestHelper.java
@@ -0,0 +1,122 @@
+package ch.admin.seco.jobroom;
+
+import ch.admin.seco.jobroom.model.*;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.sql.Date;
+import java.util.Arrays;
+
+public final class JobOfferTestHelper {
+
+    public static JobOffer getCompleteJobOffer() {
+
+        return new JobOffer(
+                null,
+                null,
+                Date.valueOf("2000-01-01"),
+                Date.valueOf("2001-02-02"),
+                new Job(
+                        "Ingénieur",
+                        "Software development",
+                        80,
+                        100,
+                        Date.valueOf("2000-03-01"),
+                        Date.valueOf("2002-04-02"),
+                        new Location(
+                                "CH",
+                                "Bern",
+                                "3010",
+                                "All day in an office"
+                        ),
+                        Arrays.asList(
+                                new LanguageSkill("de", LanguageSkill.Level.very_good, LanguageSkill.Level.good),
+                                new LanguageSkill("fr", LanguageSkill.Level.average, LanguageSkill.Level.average)
+                        )
+                ),
+                new Company(
+                        "SECO",
+                        "CH",
+                        "Finkenhubelweg",
+                        "12",
+                        "Bern",
+                        "3001",
+                        "0581234567",
+                        "info@seco.admin.ch",
+                        "www.seco.admin.ch",
+                        new Postbox(
+                                "3002",
+                                "Bern",
+                                "3001"
+                        )
+                ),
+                new Contact(
+                        Contact.Title.mister,
+                        "Jean",
+                        "Dupont",
+                        "0791234567",
+                        "jean.dupont@seco.admin.ch"
+                ),
+                new Application(
+                        false,
+                        true,
+                        true
+                )
+        );
+    }
+
+    public static JsonObject getCompleteJobOfferJson() {
+
+        JsonObject jobOffer = Json.createObjectBuilder()
+                .add("publicationStartDate", "2000-01-01")
+                .add("publicationEndDate", "2001-02-02")
+                .add("job", Json.createObjectBuilder()
+                    .add("title", "Ingénieur")
+                    .add("description", "Software development")
+                    .add("workingTimePercentageFrom", 80)
+                    .add("workingTimePercentageTo", 100)
+                    .add("startDate", "2000-03-01")
+                    .add("endDate", "2002-04-02")
+                    .add("location", Json.createObjectBuilder()
+                        .add("countryCode", "CH")
+                        .add("locality", "Bern")
+                        .add("postalCode", "3010")
+                        .add("additionalDetails", "All day in an office"))
+                    .add("languageSkills", Json.createArrayBuilder()
+                        .add(Json.createObjectBuilder()
+                            .add("language", "de")
+                            .add("spokenLevel", "very_good")
+                            .add("writtenLevel", "good"))
+                        .add(Json.createObjectBuilder()
+                            .add("language", "fr")
+                            .add("spokenLevel", "average")
+                            .add("writtenLevel", "average"))))
+                .add("company", Json.createObjectBuilder()
+                    .add("name", "SECO")
+                    .add("countryCode", "CH")
+                    .add("street", "Finkenhubelweg")
+                    .add("houseNumber", "12")
+                    .add("locality", "Bern")
+                    .add("postalCode", "3001")
+                    .add("phoneNumber", "0581234567")
+                    .add("email", "info@seco.admin.ch")
+                    .add("website", "www.seco.admin.ch")
+                    .add("postbox", Json.createObjectBuilder()
+                        .add("number", "3002")
+                        .add("locality", "Bern")
+                        .add("postalCode", "3001")))
+                .add("contact", Json.createObjectBuilder()
+                    .add("title", "mister")
+                    .add("firstName", "Jean")
+                    .add("lastName", "Dupont")
+                    .add("phoneNumber", "0791234567")
+                    .add("email", "jean.dupont@seco.admin.ch"))
+                .add("application", Json.createObjectBuilder()
+                    .add("telephonic", false)
+                    .add("written", true)
+                    .add("electronic", true))
+                .build();
+
+        return jobOffer;
+    }
+}
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,14 +1,18 @@
-INSERT INTO job_offer (id, version, title, description, company_name, company_country_code, company_locality, company_postal_code, company_street, company_house_number, start_immediate)
-VALUES (1, 0, 'John Doe', '...........hey!
-more blah,...........', 'SQL Power', 'CA', 'Montreal Nord', 'QC H1G 2V1', 'Hockey Avenue', '101', 0);
-INSERT INTO job_offer_language_skill(job_offer_id, language, spoken_level, written_level)
-VALUES (1, 'fr', 1, 0);
-INSERT INTO job_offer_language_skill(job_offer_id, language, spoken_level, written_level)
-VALUES (1, 'en', 2, 2);
+insert into JobOffer (
+  id, version, publicationstartdate, publicationenddate,
+  job_title, job_description, job_workingtimepercentagefrom, job_workingtimepercentageto, job_startdate, job_enddate,
+  job_location_countrycode, job_location_locality, job_location_postalcode, job_location_additionaldetails,
+  company_name, company_countrycode, company_street, company_housenumber, company_locality, company_postalcode, company_phonenumber, company_email, company_website, company_postbox_number, company_postbox_locality, company_postbox_postalcode,
+  contact_title, contact_firstname, contact_lastname, contact_phonenumber, contact_email,
+  application_telephonic, application_written, application_electronic
+  )
+values (1, 0, '2000-01-01', '2001-02-02', 'Ingénieur', 'Software development', 80, 100, '2000-03-01', '2002-04-02',
+  'BE', 'Bern', '3010', 'All day in an office',
+  'SECO', 'CH', 'Finkenhubelweg', '12', 'Bern', '3001', '0581234567', 'info@seco.admin.ch', 'www.seco.admin.ch', '3002', 'Bern', '3001',
+  'mister', 'Jean', 'Dupont', '0791234567', 'jean.dupont@seco.admin.ch',
+  false, true, true);
 
-INSERT INTO job_offer (id, version, title, description, company_name, company_country_code, company_locality, company_postal_code, company_street, company_house_number, start_immediate)
-VALUES (2, 0, 'Web Developer', 'SECO TCIT', 'a
-b
-c
-d
-é', 'CH', 'Bern', '3002', 'Finkenhubelweg', '12', 1);
+insert into JobOffer_job_languageskills(JobOffer_id, language, spokenlevel, writtenlevel)
+values(1, 'de', 'very_good', 'good');
+insert into JobOffer_job_languageskills(JobOffer_id, language, spokenlevel, writtenlevel)
+values(1, 'fr', 'average', 'average');
