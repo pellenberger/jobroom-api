dd33b06da8b5e6e24f3fbf28290e819a3851f4d3
pellenberger <p.ellenberger@outlook.com>
2016-07-26 08:55:53 +0200

    Security : allow access to /docs without being authenticated

--- src/main/java/ch/admin/seco/jobroom/config/SecurityConfig.java
+++ src/main/java/ch/admin/seco/jobroom/config/SecurityConfig.java
@@ -22,7 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().anyRequest().authenticated()
+                .authorizeRequests()
+                .antMatchers("/docs/**").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .and()
