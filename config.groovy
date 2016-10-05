5459ab3929a8bc14a5cb8b0b04f852dd22e7f1c6
pellenberger <p.ellenberger@outlook.com>
2016-10-05 13:41:25 +0200

    [ALV #5150] Export the correct URL in doc, depending on environment
    - 2 possible environments : staging and prod
    - travis automatically choose the right one, depending on the branch

---
+++ config.groovy
@@ -0,0 +1,11 @@
+environments {
+    staging {
+        host='www.job-room.ch'
+        basePath='/api/staging'
+    }
+
+    prod {
+        host='www.job-room.ch'
+        basePath='/api/prod'
+    }
+}
